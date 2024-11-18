f9cd703f0c99a486c323de303e6f614886a8190b
AnjaliSardana <anjali.sardana8@gmail.com>
2024-11-18 01:25:18 -0500

    price table not updated?

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.').at(1);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img';
-          }
-          return `assets/${extType}/[name][extname]`;
-        },
-      },
-    },
-  }
 })
